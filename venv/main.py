d61749aeabe92eb633e7df4514edac14c549c193
Siddharth Madhavan <79624807+HuntingHack@users.noreply.github.com>
2023-07-12 15:22:43 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -21,91 +21,89 @@ if(st.button("About")):
 
 uploaded_file = st.file_uploader("Upload your file here...")
 
-if not uploaded_file:
+log_data = pd.read_csv(uploaded_file)
 
-  log_data = pd.read_csv(uploaded_file)
+st.write(log_data)
 
-  st.write(log_data)
+# Perform initial data exploration
+print(log_data.head())  # Display the first few rows of the dataset
+print(log_data.info())  # Get information about the dataset
 
-  # Perform initial data exploration
-  print(log_data.head())  # Display the first few rows of the dataset
-  print(log_data.info())  # Get information about the dataset
-
-  # Create new columns for day, month, year, and time
-  log_data['Date'] = log_data['Time'].str.extract(r'\[(\d{2}/\w+/\d{4})')
-  log_data['Day'] = log_data['Date'].str.extract(r'(\d{2})/')
-  log_data['Month'] = log_data['Date'].str.extract(r'/(\w+)/')
-  log_data['Year'] = log_data['Date'].str.extract(r'/(\d{4})')
-  log_data['Time'] = log_data['Time'].str.extract(r':(\d{2}:\d{2}:\d{2})')
-  log_data['URL'] = log_data['URL'].str.extract(r'(\S+)\sHTTP/1\.1')
+# Create new columns for day, month, year, and time
+log_data['Date'] = log_data['Time'].str.extract(r'\[(\d{2}/\w+/\d{4})')
+log_data['Day'] = log_data['Date'].str.extract(r'(\d{2})/')
+log_data['Month'] = log_data['Date'].str.extract(r'/(\w+)/')
+log_data['Year'] = log_data['Date'].str.extract(r'/(\d{4})')
+log_data['Time'] = log_data['Time'].str.extract(r':(\d{2}:\d{2}:\d{2})')
+log_data['URL'] = log_data['URL'].str.extract(r'(\S+)\sHTTP/1\.1')
 
 
-  page_views = log_data['URL'].value_counts()  # Count of page views for each URL
+page_views = log_data['URL'].value_counts()  # Count of page views for each URL
 
-  # Data Visualization
+# Data Visualization
 
-  st.subheader('Data Visualization:')
-  fig = px.line(page_views, title='Page Views')
-  st.plotly_chart(fig)
+st.subheader('Data Visualization:')
+fig = px.line(page_views, title='Page Views')
+st.plotly_chart(fig)
 
-  aggregated_data = log_data.groupby('Date').agg({'IP': 'count'}).reset_index()
-  aggregated_data.columns = ['Date', 'request_count']
+aggregated_data = log_data.groupby('Date').agg({'IP': 'count'}).reset_index()
+aggregated_data.columns = ['Date', 'request_count']
 
-  aggregated_data.set_index('Date', inplace=True)
-  target_variable = aggregated_data['request_count']
+aggregated_data.set_index('Date', inplace=True)
+target_variable = aggregated_data['request_count']
 
-  target_variable.plot(figsize=(12, 6))
-  plt.xlabel('Timestamp')
-  plt.ylabel('Total Requests')
-  plt.title('Web Log Data')
-  plt.show()
+target_variable.plot(figsize=(12, 6))
+plt.xlabel('Timestamp')
+plt.ylabel('Total Requests')
+plt.title('Web Log Data')
+plt.show()
 
-  # Step 3: Make the Time Series Stationary
-  # Apply differencing to remove trend and seasonality
-  differenced_data = target_variable.diff().dropna()
+# Step 3: Make the Time Series Stationary
+# Apply differencing to remove trend and seasonality
+differenced_data = target_variable.diff().dropna()
 
-  # Step 4: Determine ARIMA Parameters
-  # Plot the autocorrelation and partial autocorrelation functions
-  fig, ax = plt.subplots(figsize=(12, 6))
-  plot_acf(differenced_data, ax=ax, lags=30)
-  plt.xlabel('Lags')
-  plt.ylabel('Autocorrelation')
-  plt.title('Autocorrelation Function')
-  plt.show()
+# Step 4: Determine ARIMA Parameters
+# Plot the autocorrelation and partial autocorrelation functions
+fig, ax = plt.subplots(figsize=(12, 6))
+plot_acf(differenced_data, ax=ax, lags=30)
+plt.xlabel('Lags')
+plt.ylabel('Autocorrelation')
+plt.title('Autocorrelation Function')
+plt.show()
 
-  fig, ax = plt.subplots(figsize=(12, 6))
-  plot_pacf(differenced_data, ax=ax, lags=25)
-  plt.xlabel('Lags')
-  plt.ylabel('Partial Autocorrelation')
-  plt.title('Partial Autocorrelation Function')
-  plt.show()
+fig, ax = plt.subplots(figsize=(12, 6))
+plot_pacf(differenced_data, ax=ax, lags=25)
+plt.xlabel('Lags')
+plt.ylabel('Partial Autocorrelation')
+plt.title('Partial Autocorrelation Function')
+plt.show()
 
-  # Determine the order (p, d, q) of the ARIMA model based on the plots and ADF test
+# Determine the order (p, d, q) of the ARIMA model based on the plots and ADF test
 
-  # Step 5: Split the Data
-  train_data = differenced_data[:-30]  # Use all but the last 30 data points for training
-  test_data = differenced_data[-30:]  # Use the last 30 data points for testing
+# Step 5: Split the Data
+train_data = differenced_data[:-30]  # Use all but the last 30 data points for training
+test_data = differenced_data[-30:]  # Use the last 30 data points for testing
 
-  # Step 6: Fit the ARIMA Model
+# Step 6: Fit the ARIMA Model
 
 
-  order = (2, 3, 1)  # Example order, replace with the appropriate values
-  model = ARIMA(train_data, order=order)
-  model_fit = model.fit()
-  # Step 8: Evaluate the Model
-  predictions = model_fit.forecast(steps=len(test_data))
-  mse = mean_squared_error(test_data, predictions)
-  rmse = np.sqrt(mse)
-  mae = mean_absolute_error(test_data, predictions)
-  print(f"RMSE: {rmse:.2f}")
-  print(f"MAE: {mae:.2f}")
+order = (2, 3, 1)  # Example order, replace with the appropriate values
+model = ARIMA(train_data, order=order)
+model_fit = model.fit()
+# Step 8: Evaluate the Model
+predictions = model_fit.forecast(steps=len(test_data))
+mse = mean_squared_error(test_data, predictions)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(test_data, predictions)
+print(f"RMSE: {rmse:.2f}")
+print(f"MAE: {mae:.2f}")
 
-  # Step 9: Forecast with the Model
+# Step 9: Forecast with the Model
 
-  inpu2 = st.text_input("Enter the required number of predictions:")
+inpu2 = st.text_input("Enter the required number of predictions:")
 
-  if st.button('Submit'):
-    pred_no = inpu2.title()
+if st.button('Submit'):
+ pred_no = inpu2.title()
 
 future_predictions = model_fit.forecast(steps=int(pred_no))  # Example: Generate 10 future predictions
 st.subheader('Predictions:')
