391a630b95050f2d2f79b106caee82b85f7d06f9
Siddharth Madhavan <79624807+HuntingHack@users.noreply.github.com>
2023-07-15 17:05:00 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -117,9 +117,8 @@ train_data = differenced_data[:-30]  # Use all but the last 30 data points for t
 test_data = differenced_data[-30:]  # Use the last 30 data points for testing
 
 # Step 6: Fit the ARIMA Model
-
-
 order = (0, 0, 1)
+model = ARIMA(train_data, order=order)
 model_fit = model.fit()
 # Step 8: Evaluate the Model
 predictions = model_fit.forecast(steps=len(test_data))
