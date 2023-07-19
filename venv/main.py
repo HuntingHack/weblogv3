78187952c976c99b741ba46c6802bb4bdf260da2
Siddharth Madhavan <79624807+HuntingHack@users.noreply.github.com>
2023-07-19 16:34:59 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -10,11 +10,9 @@ import plotly.graph_objects as go
 import plotly.express as px
 import time
 
-emp = st.empty()
-    
 # Code for website design
 
-with emp.form('Disclaimer'):
+with (emp = st.form('Disclaimer')):
   st.subheader('Disclaimer')
   st.write('[T&Cs](https://docs.google.com/document/d/1ej4FDVM_NPhB3BDbsRZ4ygkt738de62WUlap42gy4ZY/edit?usp=sharing)')
   st.write('[Privacy Policy](https://docs.google.com/document/d/1d5BDiJyufvRkGjB0BjwW3PGpdCEUKi2MYFkL4B1UKe0/edit?usp=sharing)')
