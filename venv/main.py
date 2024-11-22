dd57c8320a8757ece08c49c0f2e5cf36fa1bc7f6
Siddharth Madhavan <79624807+HuntingHack@users.noreply.github.com>
2024-11-22 20:08:15 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -137,7 +137,7 @@ if (button1 == 'Agree'):
 
         #Fit the ARIMA Model
 
-        order = (12, 2, 5)
+        order = (12, 8, 5)
         model = ARIMA(train_data, order=order).fit()
         #Evaluate the Model
         predictions = model.forecast(30)
